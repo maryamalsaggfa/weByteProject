8a4ecdbd3c7b51e20847855aab8fdafe519d6929
maryamalsaggfa <maryam1xsa@gmail.com>
2023-10-08 07:29:26 -0700

    Initial Commit

---
+++ weByteProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  weByteProject
+//
+//  Created by maryam on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ weByteProject/weByteProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  weByteProjectApp.swift
+//  weByteProject
+//
+//  Created by maryam on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct weByteProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
