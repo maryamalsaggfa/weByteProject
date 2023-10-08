6275d164bc0d61944ebc581344231ac90483be7c
maryamalsaggfa <maryam1xsa@gmail.com>
2023-10-08 07:37:23 -0700

    commit 2

--- weByteProject/ContentView.swift
+++ weByteProject/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("We Byte Team")
+            Text("We Byte Team Cool")
         }
         .padding()
     }
