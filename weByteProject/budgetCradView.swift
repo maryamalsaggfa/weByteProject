774f7117ea847b62047c8e73e0594c5576505abd
maryamalsaggfa <maryam1xsa@gmail.com>
2023-10-08 13:51:42 -0700

    commit done

---
+++ weByteProject/budgetCradView.swift
@@ -0,0 +1,18 @@
+//
+//  budgetCradView.swift
+//  weByteProject
+//
+//  Created by maryam on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+struct budgetCradView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    budgetCradView()
+}
---
+++ weByteProject/budgetView.swift
@@ -0,0 +1,18 @@
+//
+//  budgetView.swift
+//  weByteProject
+//
+//  Created by maryam on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+struct budgetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    budgetView()
+}
