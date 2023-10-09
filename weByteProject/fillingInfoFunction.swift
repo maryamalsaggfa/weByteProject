83ea2a8bc132280c448fa8a646f1aafecf54203c
maryamalsaggfa <maryam1xsa@gmail.com>
2023-10-09 02:23:15 -0700

    commit experssions

---
+++ weByteProject/fillingInfoFunction.swift
@@ -0,0 +1,18 @@
+//
+//  fillingInfoFunction.swift
+//  weByteProject
+//
+//  Created by maryam on 24/03/1445 AH.
+//
+
+import SwiftUI
+
+struct fillingInfoFunction: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    fillingInfoFunction()
+}
