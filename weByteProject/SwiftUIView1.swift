237456a4728b95103e973728c59284bdc5b36003
Itsmeamani <itsme.amani@icloud.com>
2023-10-09 22:30:25 -0700

    mmmm

---
+++ weByteProject/SwiftUIView1.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView1.swift
+//  weByteProject
+//
+//  Created by Amani on 24/03/1445 AH.
+//
+
+import SwiftUI
+
+struct SwiftUIView1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView1()
+}
