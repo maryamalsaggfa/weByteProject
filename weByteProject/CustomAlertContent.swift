cb711ed29f49f8a5022a5ff86c7dc57a739b7e88
maryamalsaggfa <maryam1xsa@gmail.com>
2023-10-11 01:06:11 -0700

    save expeness maryam

---
+++ weByteProject/CustomAlertContent.swift
@@ -0,0 +1,18 @@
+//
+//  CustomAlertContent.swift
+//  weByteProject
+//
+//  Created by maryam on 26/03/1445 AH.
+//
+
+import SwiftUI
+
+struct CustomAlertContent: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomAlertContent()
+}
