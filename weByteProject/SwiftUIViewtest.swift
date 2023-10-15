8a329f053e4a3bd63e9d2027a829a6f5f91e8ac8
s0011x <suha.alrajhi01@gmail.com>
2023-10-15 15:53:44 +0300

    suha

---
+++ weByteProject/SwiftUIViewtest.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIViewtest.swift
+//  weByteProject
+//
+//  Created by suha alrajhi on 30/03/1445 AH.
+//
+
+import SwiftUI
+
+struct SwiftUIViewtest: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIViewtest()
+}
