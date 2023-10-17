ef19b2873edfd4f740d829caee265620e942f9a4
maryamalsaggfa <maryam1xsa@gmail.com>
2023-10-17 01:41:12 -0700

    last project to show

---
+++ weByteProject/RoadMap.swift
@@ -0,0 +1,18 @@
+//
+//  RoadMap.swift
+//  weByteProject
+//
+//  Created by maryam on 01/04/1445 AH.
+//
+
+import SwiftUI
+
+struct RoadMap: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RoadMap()
+}
---
+++ weByteProject/splash.swift
@@ -0,0 +1,18 @@
+//
+//  splash.swift
+//  weByteProject
+//
+//  Created by maryam on 01/04/1445 AH.
+//
+
+import SwiftUI
+
+struct splash: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    splash()
+}
